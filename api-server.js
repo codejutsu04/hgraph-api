86b0170f327bcdc3b8d7699be6541e4512d5b753
codejutsu-desu <mrimmoys@gmail.com>
2025-09-14 15:57:45 +0600

    Update server port to 3009

--- api-server.js
+++ api-server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3009;
 
 app.use(cors());
 app.use(express.json());
